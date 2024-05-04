90e32a551dccd136eb6765ef423372e4ed0a8241
Todd J. Lee <tjltm83@gmail.com>
2024-05-03 19:34:46 -0700

    still filling things out.

--- HolleyCAN.py
+++ HolleyCAN.py
@@ -1,12 +1,25 @@
 import warnings
 
 class Parser():
-    def __init__(self, SubScribedFunctions = 'All'):
+    def __init__(self, SerialNumber, SubScribedFunctions = 'All'):
         '''
         format CANID to function to parse the data
         because this is just weird ass stuff 
         '''
+        
+        '''
+        From the document the CANId includes the SerialNumber which must be Filtered For 
+        '''
+        self.__SerialNumber = SerialNumber
+        self.__SNMask = '\xff\xff\xf8\x00'
+
+        '''
+        Certain Subscriptions need data type specified 
+        '''
         self.__Subscriptions = []
+        self.__NeedsSpecialAttention = ['1E051000','1E055000','1E059000','1E05D000','1E067000']
+        self.__ValidIODataTypes = {'bool', "duty_cycle", "msec"}
+        
         self.__Map = {
             '1E001000' : {'Function':self.__RTCAndRPM,'Index':0,},
             '1E005000' : {'Function':self.__FuelFLowAndInjector,'Index':1,},
@@ -28,11 +41,11 @@ class Parser():
             '1E045000' : {'Function':self.__GearAndN20S4,'Index':17,},
             '1E049000' : {'Function':self.__LinePressureAndSpeed,'Index':18,},
             '1E04D000' : {'Function':self.__InputShaftSpeedAndLineTemp,'Index':19,},
-            '1E051000' : {'Function':self.__Input1And2,'Index':20,},
-            '1E055000' : {'Function':self.__Input3And4,'Index':21,},
-            '1E059000' : {'Function':self.__Output1AndInput5,'Index':22,},
-            '1E05D000' : {'Function':self.__Output2And3,'Index':23,},
-            '1E067000' : {'Function':self.__Output4And5,'Index':24,},
+            '1E051000' : {'Function':self.__Input1And2,'Index':20,"DefinedAs":'bool'},
+            '1E055000' : {'Function':self.__Input3And4,'Index':21,"DefinedAs":'bool'},
+            '1E059000' : {'Function':self.__Output1AndInput5,'Index':22,"DefinedAs":'bool'},
+            '1E05D000' : {'Function':self.__Output2And3,'Index':23,"DefinedAs":'bool'},
+            '1E067000' : {'Function':self.__Output4And5,'Index':24,"DefinedAs":'bool'},
         }
 
         #GenerateDefaultSubscribeList 
@@ -42,7 +55,9 @@ class Parser():
         else:
             self.__Subscriptions = SubScribedFunctions
 
-        
+    @property
+    def SerialNumber(self):
+        return self.__SerialNumber
 
     @property
     def Subscriptions(self):
@@ -60,80 +75,155 @@ class Parser():
         else:
             warnings.warn("Not Subscribed: {}".format(Data))
 
+    def SetIOParseType(self, IOSub, DataType):
+        if IOSub in self.__NeedsSpecialAttention:
+            if DataType in self.__ValidIODataTypes:
+                self.__Map[IOSub]["DefinedAs"] == DataType
+            else: 
+                warnings.warn('IOSub::{0} has invalid datatype: {1} ::These are valid:'.format(IOSub,DataType,self.__ValidIODataTypes))
+        else:
+            warnings.warn("IOSub invalid:: {0} ::These are valid:: {1}".format(IOSub, self.__NeedsSpecialAttention))
+
+    @property
+    def IODataType(self,IOSub):
+        if IOSub in self.__NeedsSpecialAttention:
+            return self.__Map[IOSub]["DefinedAs"]
+        else:
+            warnings.warn("IOSub property invalid:: {0} ::These are valid:: {1}".format(IOSub, self.__NeedsSpecialAttention))
+            return None
+
     def DataToParse(self,DataPacket):
         pass
 
+    def __FloatConversion(self,Byte):
+        '''
+        This API uses signed fixed point format
+        '''
+
+        Value = Byte/256
+        return Value
+
     def __RTCAndRPM(self,Data):
-        pass
+        '''
+        '''
+        ReturnValueNames = {'Lower':'RTC','Upper':'RPM'}
 
     def __FuelFLowAndInjector(self,Data):
-        pass
+        '''
+        '''
+        ReturnValueNames = {'Lower':'Injector Pulsewidth','Upper':'Fuel FLow'}
 
     def __CLStatusAndDutyC(self,Data):
-        pass
+        '''
+        '''
+        ReturnValueNames = {'Lower':'Closed Loop Status','Upper':'Duty Cycle'}
 
     def __AFRLandCLC(self,Data):
-        pass
+        '''
+        '''
+        ReturnValueNames = {'Lower':'AFR Left','Upper':'Closed Loop Compensation'}
 
     def __TargetAFRAndAFRR(self,Data):
-        pass
+        '''
+        '''
+        ReturnValueNames = {'Lower':'Target AFR','Upper':'AFR Right'}
 
     def __IgTimeAndAFRAVE(self,Data):
-        pass
+        '''
+        '''
+        ReturnValueNames = {'Lower':'Ignition Timing','Upper':'AFR Average'}
 
     def __MAPandKtard(self,Data):
-        pass
+        '''
+        '''
+        ReturnValueNames = {'Lower':'MAP','Upper':'Knock Retard'}
 
     def __MATandTPS(self,Data):
-        pass
+        '''
+        '''
+        ReturnValueNames = {'Lower':'MAT','Upper':'TPS'}
 
     def __BaroAndCTS(self,Data):
-        pass
+        '''
+        '''
+        ReturnValueNames = {'Lower':'Barometric Pressure','Upper':'CTS'}
 
     def __OilAndVoltage(self,Data):
-        pass
+        '''
+        '''
+        ReturnValueNames = {'Lower':'Oil Pressure','Upper':'Battery Voltage'}
 
     def __PedalPosAndFuelPress(self,Data):
-        pass
+        '''
+        '''
+        ReturnValueNames = {'Lower':'Pedal Position','Upper':'Fuel Pressure'}
 
     def __BoostGearAndRevLimit(self,Data):
-        pass
+        '''
+        '''
+        ReturnValueNames = {'Lower':'Boost Gear','Upper':'Main Rev Limit'}
 
     def __BoostSpeedAndStage(self,Data):
-        pass
+        '''
+        '''
+        ReturnValueNames = {'Lower':'Boost Speed','Upper':'Boost Stage'}
 
     def __BoostTargetAndTime(self,Data):
-        pass
+        '''
+        '''
+        ReturnValueNames = {'Lower':'Target Boost','Upper':'Boost Time'}
 
     def __BoostAndSolenoid(self,Data):
-        pass
+        '''
+        '''
+        ReturnValueNames = {'Lower':'Boost Solenoid Duty Cycle','Upper':'Boost'}
 
     def __N20S1AndWaterMeth(self,Data):
-        pass
+        '''
+        '''
+        ReturnValueNames = {'Lower':'N20 Stage 1','Upper':'Water Meth Injection'}
 
     def __N20S2andS3(self,Data):
-        pass
+        '''
+        '''
+        ReturnValueNames = {'Lower':'N20 Stage 3','Upper':'N20 Stage 2'}
 
     def __GearAndN20S4(self,Data):
-        pass
+        '''
+        '''
+        ReturnValueNames = {'Lower':'Gear','Upper':'N20 Stage 4'}
 
     def __LinePressureAndSpeed(self,Data):
-        pass
+        '''
+        '''
+        ReturnValueNames = {'Lower':'Line Pressure','Upper':'Speed'}
 
     def __InputShaftSpeedAndLineTemp(self,Data):
-        pass
+        '''
+        '''
+        ReturnValueNames = {'Lower':'Input Shaft Speed','Upper':'Line Temp'}
 
     def __Input1And2(self,Data):
-        pass
+        '''
+        '''
+        ReturnValueNames = {'Lower':'Input 2','Upper':'Input 1'}
 
     def __Input3And4(self,Data):
-        pass
+        '''
+        '''
+        ReturnValueNames = {'Lower':'Input 4','Upper':'Input 3'}
 
     def __Output1AndInput5(self,Data):
-        pass
+        '''
+        '''
+        ReturnValueNames = {'Lower':'Output 1','Upper':'Input 5'}
 
     def __Output2And3(self,Data):
-        pass
+        '''
+        '''
+        ReturnValueNames = {'Lower':'Output 3','Upper':'Output 2'}
 
     def __Output4And5(self,Data):
-        pass
+        '''
+        '''
+        ReturnValueNames = {'Lower':'Output 5','Upper':'Output 4'}
---
+++ TEST.py
@@ -0,0 +1,5 @@
+import HolleyCAN
+
+Test = HolleyCAN.Parser('00')
+print(Test.SerialNumber)
+print(Test.Subscriptions)
